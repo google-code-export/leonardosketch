af4bb2532819f98f43126236bda4880282b60800
Josh Marinacci <joshua@marinacci.org>
2010-11-29 12:25:50 -0800

    added prefs to enable the debug menu

--- Sketch/src/org/joshy/sketch/actions/PreferencesAction.java
+++ Sketch/src/org/joshy/sketch/actions/PreferencesAction.java
@@ -4,6 +4,7 @@ import org.joshy.gfx.event.ActionEvent;
 import org.joshy.gfx.event.Callback;
 import org.joshy.gfx.node.control.Button;
 import org.joshy.gfx.node.control.Checkbox;
+import org.joshy.gfx.node.control.Label;
 import org.joshy.gfx.node.control.Linkbutton;
 import org.joshy.gfx.node.layout.HFlexBox;
 import org.joshy.gfx.node.layout.Spacer;
@@ -36,6 +37,8 @@ public class PreferencesAction extends SAction {
     @Override
     public void execute() throws Exception {
         final Stage stage = Stage.createStage();
+        
+        //tracking checkbox
         boolean trackingEnabled = "true".equals(manager.settings.getProperty(Main.TRACKING_PERMISSIONS));
         Checkbox trackingCheckbox = new Checkbox(getString("preferences.enable.analytics.tracking"));
         trackingCheckbox.setSelected(trackingEnabled);
@@ -45,6 +48,17 @@ public class PreferencesAction extends SAction {
                     Main.settings.setProperty(Main.TRACKING_PERMISSIONS,""+checkbox.isSelected());
                 }
             });
+
+        //debug menu
+        Checkbox debugMenuCheckbox = new Checkbox(getString("preferences.enable.debug.menu"));
+        boolean debugMenuEnabled = "true".equals(manager.settings.getProperty(Main.DEBUG_MENU));
+        debugMenuCheckbox.setSelected(debugMenuEnabled);
+        debugMenuCheckbox.onClicked(new Callback<ActionEvent>(){
+            public void call(ActionEvent actionEvent) throws Exception {
+                Checkbox checkbox = (Checkbox) actionEvent.getSource();
+                Main.settings.setProperty(Main.DEBUG_MENU,""+checkbox.isSelected());
+            }
+        });
 
         Callback<ActionEvent> closeAction = new Callback<ActionEvent>() {
             public void call(ActionEvent actionEvent) throws Exception {
@@ -60,6 +74,10 @@ public class PreferencesAction extends SAction {
                     OSUtil.openBrowser("http://code.google.com/p/leonardosketch/wiki/Tracking");
                 }
             }))
+        );
+        tab.add("debug", new VFlexBox().setBoxAlign(VFlexBox.Align.Stretch)
+            .add(debugMenuCheckbox)
+            .add(new Label(getString("misc.changesAppliedLater")))
         );
 
         stage.setContent(new VFlexBox().setBoxAlign(VFlexBox.Align.Stretch)
