9cbe1c7471700168803b3cf35eed953c97f640ae
Josh Marinacci <joshua@marinacci.org>
2011-01-17 17:10:47 -0800

    adding pixel stuff

---
+++ Sketch/src/org/joshy/sketch/pixel/model/PixelDocTest.java
@@ -0,0 +1,16 @@
+package org.joshy.sketch.pixel.model;
+
+import org.joshy.gfx.Core;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: joshmarinacci
+ * Date: 1/17/11
+ * Time: 5:10 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class PixelDocTest {
+    public static void main(String ... args) throws Exception {
+        Core.init();
+    }
+}
---
+++ Sketch/src/org/joshy/sketch/pixel/model/PixelLayer.java
@@ -0,0 +1,14 @@
+package org.joshy.sketch.pixel.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: joshmarinacci
+ * Date: 1/17/11
+ * Time: 5:10 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class PixelLayer {
+    public PixelLayer() {
+        new PixelDocTest();
+    }
+}
