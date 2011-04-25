5a576276749133feb340d19d1e501d63532f3fa7
Josh Marinacci <joshua@marinacci.org>
2011-04-25 16:18:34 -0700

    updated gradient handles with new color picker

--- Sketch/src/org/joshy/sketch/canvas/BaseGradientHandle.java
+++ Sketch/src/org/joshy/sketch/canvas/BaseGradientHandle.java
@@ -1,12 +1,16 @@
 package org.joshy.sketch.canvas;
 
 import org.joshy.gfx.draw.*;
+import org.joshy.gfx.draw.Image;
 import org.joshy.gfx.event.Callback;
 import org.joshy.gfx.event.ChangedEvent;
+import org.joshy.gfx.event.EventBus;
 import org.joshy.gfx.event.MouseEvent;
+import org.joshy.gfx.node.NodeUtils;
+import org.joshy.gfx.node.control.Button;
 import org.joshy.gfx.node.control.Control;
-import org.joshy.gfx.node.control.SwatchColorPicker;
 import org.joshy.gfx.node.layout.Container;
+import org.joshy.gfx.stage.Stage;
 import org.joshy.gfx.util.GeomUtil;
 import org.joshy.sketch.model.AbstractResizeableNode;
 import org.joshy.sketch.model.Handle;
@@ -16,9 +20,11 @@ import org.joshy.sketch.tools.MouseEventHandle;
 import org.joshy.sketch.util.DrawUtils;
 import org.joshy.sketch.util.Util;
 
+import java.awt.*;
 import java.awt.geom.Line2D;
 import java.awt.geom.Path2D;
 import java.awt.geom.Point2D;
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -65,7 +71,7 @@ public abstract class BaseGradientHandle<G extends MultiGradientFill>
     }
 
     protected void addStopControl(final RadialGradientFill.Stop stop) {
-        SwatchColorPicker colorPopup = new CustomSwatchColorPicker();
+        CustomSwatchColorPicker colorPopup = new CustomSwatchColorPicker();
         colorPopup.setPrefWidth(10);
         colorPopup.setPrefHeight(10);
         colorPopup.setSelectedColor(stop.getColor());
@@ -288,7 +294,33 @@ public abstract class BaseGradientHandle<G extends MultiGradientFill>
 
     protected abstract Point2D getEnd();
 
-    private class CustomSwatchColorPicker extends SwatchColorPicker {
+    private class CustomSwatchColorPicker extends Button {
+        FlatColor selectedColor = FlatColor.RED;
+        private PopupColorPicker popup;
+
+        @Override
+        protected void setPressed(boolean pressed) {
+            super.setPressed(pressed);
+            if (pressed) {
+                if (popup == null) {
+                    popup = new PopupColorPicker(this,200,100);
+                    popup.setVisible(false);
+                    Stage stage = getParent().getStage();
+                    stage.getPopupLayer().add(popup);
+                }
+                Point2D pt = NodeUtils.convertToScene(this, 0, getHeight());
+
+                double x = pt.getX();
+                double y = pt.getY();
+                FlatColor color = this.getSelectedColor();
+                popup.positionAt(x, y, color);
+                popup.setVisible(true);
+                EventBus.getSystem().setPressedNode(popup);
+            } else {
+                popup.setVisible(false);
+            }
+        }
+
         @Override
         public void draw(GFX g) {
             if(!isVisible())return;
@@ -297,9 +329,127 @@ public abstract class BaseGradientHandle<G extends MultiGradientFill>
             g.fillOval(0, 0, getWidth(), getHeight());
             g.setPaint(FlatColor.WHITE);
             g.fillOval(0 + 1, 0 + 1, getWidth() - 2, getHeight() - 2);
-            g.setPaint(getSelectedColor());
+            g.setPaint(selectedColor);
             g.fillOval(0 + 2, 0 + 2, getWidth() - 4, getHeight() - 4);
             g.setPureStrokes(false);
+        }
+
+        public void setSelectedColor(FlatColor color) {
+            this.selectedColor = color;
+            EventBus.getSystem().publish(new ChangedEvent(ChangedEvent.ColorChanged,selectedColor,this));
+            setDrawingDirty();
+        }
+
+        public void onColorSelected(Callback<ChangedEvent> callback) {
+            EventBus.getSystem().addListener(this, ChangedEvent.ColorChanged, callback);
+        }
+
+        public FlatColor getSelectedColor() {
+            return selectedColor;
+        }
+        private class PopupColorPicker extends Control {
+            private Image image;
+            private BufferedImage img;
+
+            public PopupColorPicker(CustomSwatchColorPicker swatchColorPicker, int width, int height) {
+                EventBus.getSystem().addListener(this, MouseEvent.MouseAll, new Callback<MouseEvent>() {
+                    public void call(MouseEvent event) {
+                        processMouse(event);
+                    }
+                });
+
+                img = new BufferedImage(width,height,BufferedImage.TYPE_INT_RGB);
+                Graphics2D g = img.createGraphics();
+                for(int x=0; x<width; x++) {
+                    for(int y=0; y<height; y++) {
+                        double hue = (double)x/(double)width*360.0;
+                        double sat = 1.0;
+                        double bri = 1.0;
+                        if (y < height/2) {
+                            sat = (double)y/ (height/2.0);
+                        } else {
+                            bri = 1.0-(double)(y-height/2)/(height/2.0);
+                        }
+                        FlatColor c =  FlatColor.hsb(hue,sat,bri);
+                        g.setPaint(new java.awt.Color(c.getRGBA()));
+                        g.fillRect(x,y,1,1);
+                    }
+                }
+                g.dispose();
+                image = Image.create(img);
+            }
+
+            private void processMouse(MouseEvent event) {
+                if (event.getType() == MouseEvent.MouseDragged) {
+                    int x = (int) event.getX();
+                    int y = (int) event.getY();
+                    if(x < 0 || x > img.getWidth()-1 || y <0 || y > img.getHeight()-1 ) {
+                        setSelectedColor(FlatColor.RED);
+                        setDrawingDirty();return;
+                    }
+                    setSelectedColor(new FlatColor(img.getRGB(x,y)));
+                    setDrawingDirty();
+                }
+                if (event.getType() == MouseEvent.MouseReleased) {
+                    setDrawingDirty();
+                    setVisible(false);
+                    CustomSwatchColorPicker.this.setFinalColor(getSelectedColor());
+                }
+
+            }
+
+            @Override
+            public void doPrefLayout() {
+                //noop
+            }
+
+            @Override
+            public void doLayout() {
+                setWidth(img.getWidth());
+                setHeight(img.getHeight());
+            }
+
+            @Override
+            public void doSkins() {
+            }
+
+            @Override
+            public void draw(GFX g) {
+                if (!isVisible()) return;
+                g.setPaint(FlatColor.WHITE);
+                g.fillRect(0, 0, getWidth(), getHeight());
+                g.drawImage(image,0,0);
+                Point2D pt = colorToPoint(getSelectedColor());
+                g.setPaint(FlatColor.BLACK);
+                g.drawOval(pt.getX()-5,pt.getY()-5,10,10);
+                g.setPaint(FlatColor.WHITE);
+                g.drawOval(pt.getX()-4,pt.getY()-4,8,8);
+            }
+
+            public Point2D colorToPoint(FlatColor color) {
+                //u.p("hue = " + color.getHue() + " " + getWidth() + " sat = " + color.getSaturation() + " brig = " + color.getBrightness());
+                double sat = color.getSaturation();
+                double bri = color.getBrightness();
+                double dx = color.getHue()/360.0*img.getWidth();
+
+                double dy = 0;
+                double hh = img.getHeight()/2.0;
+                if(sat < 1.0) {
+                    dy = sat*hh;
+                } else {
+                    dy = hh + (1.0-bri)*hh;
+                }
+
+                return new Point2D.Double(dx,dy);
+            }
+            public void positionAt(double x, double y, FlatColor color) {
+                Point2D pt = colorToPoint(color);
+                setTranslateX(x-pt.getX()+5);
+                setTranslateY(y-pt.getY()-5);
+            }
+        }
+
+        private void setFinalColor(FlatColor selectedColor) {
         }
     }
 }
