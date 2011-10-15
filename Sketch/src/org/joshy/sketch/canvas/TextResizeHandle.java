352c0df8a5285b1f25553a80b7d39b6dfa5a2208
Josh Marinacci <joshua@marinacci.org>
2011-10-14 22:06:13 -0700

    improved text resize handler to work with transforms as well

--- Sketch/src/org/joshy/sketch/canvas/TextResizeHandle.java
+++ Sketch/src/org/joshy/sketch/canvas/TextResizeHandle.java
@@ -3,11 +3,8 @@ package org.joshy.sketch.canvas;
 import org.joshy.sketch.model.SText;
 
 /**
- * Created by IntelliJ IDEA.
- * User: joshmarinacci
- * Date: 4/7/11
- * Time: 8:22 PM
- * To change this template use File | Settings | File Templates.
+ * Overrides the standard ResizeHandle for one purpose:
+ * to disable autosizing whenever the user manually resizes the text bounds
  */
 public class TextResizeHandle extends ResizeHandle {
     public TextResizeHandle(SText node, PositionHandle.Position position) {
@@ -16,7 +13,13 @@ public class TextResizeHandle extends ResizeHandle {
 
     @Override
     public void setX(double x, boolean constrain) {
-        super.setX(x, constrain);    //To change body of overridden methods use File | Settings | File Templates.
+        super.setX(x, constrain);
+        ((SText)this.getResizeableNode()).setAutoSize(false);
+    }
+
+    @Override
+    public void setXY(double x, double y, boolean constrain) {
+        super.setXY(x, y, constrain);
         ((SText)this.getResizeableNode()).setAutoSize(false);
     }
 }
