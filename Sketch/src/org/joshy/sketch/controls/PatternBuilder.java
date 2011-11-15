8c071d359e31d77e0d20955f7b7409d5c1155d0d
Josh Marinacci <joshua@marinacci.org>
2011-11-14 20:43:11 -0800

    improved pattern builder to generate plain patterns with noise

--- Sketch/src/org/joshy/sketch/controls/PatternBuilder.java
+++ Sketch/src/org/joshy/sketch/controls/PatternBuilder.java
@@ -38,6 +38,8 @@ public class PatternBuilder extends VFlexBox {
     private PatternPaint pat;
     private PatternPaint noisePat;
     private Slider noiseAmount;
+    private Checkbox useStripe;
+    private VFlexBox stripes;
 
     public static void main(String ... args) throws Exception {
         Core.init();
@@ -66,42 +68,58 @@ public class PatternBuilder extends VFlexBox {
 
     public PatternBuilder() {
 
+
         color1 = new FreerangeColorPicker();
         color1.setPrefWidth(30);
         color1.setPrefHeight(30);
         color1.setSelectedColor(FlatColor.BLUE);
         color1.setRecenterOnSelect(false);
-        this.add(color1, 0);
+        add(new HFlexBox()
+                .add(new Label("Base color"),0)
+                .add(color1,0)
+                ,0);
+
+        useStripe = new Checkbox("Stripe");
+        add(useStripe,0);
+
+
+        stripes = new VFlexBox();
+        stripes.setFill(FlatColor.hsb(0,0,0.8));
 
         color2 = new FreerangeColorPicker();
         color2.setPrefWidth(30);
         color2.setPrefHeight(30);
         color2.setSelectedColor(FlatColor.WHITE);
         color2.setRecenterOnSelect(false);
-        this.add(color2,0);
+        stripes.add(new HFlexBox()
+                .add(new Label("stripe color"), 0)
+                .add(color2, 0)
+                , 0);
 
         //stripes vs glyph vs plain
         //stripe width vs glyph picker
         width1 = new Slider(false);
         width1.setMin(1).setMax(64).setValue(32);
-        add(new HFlexBox()
+        stripes.add(new HFlexBox()
                 .add(new Label("stripe 1 width"), 0)
                 .add(width1, 0)
                 ,0);
 
         width2 = new Slider(false);
         width2.setMin(1).setMax(64).setValue(32);
-        add(new HFlexBox()
+        stripes.add(new HFlexBox()
                 .add(new Label("stripe 2 width"),0)
                 .add(width2,0)
                 ,0);
 
         stripeAngle = new Slider(false);
         stripeAngle.setMin(0).setMax(90).setValue(0);
-        add(new HFlexBox()
+        stripes.add(new HFlexBox()
                 .add(new Label("stripe angle"), 0)
                 .add(stripeAngle, 0)
                 , 0);
+
+        add(stripes,0);
 
         noiseAmount = new Slider(false);
         noiseAmount.setMin(0).setMax(100).setValue(0);
@@ -150,56 +168,63 @@ public class PatternBuilder extends VFlexBox {
             g2.setPaint(FlatColor.WHITE);
             g2.fillRect(0, 0, getWidth(), getHeight());
 
-            //int angle = (int) GeomUtil.snapTo45(stripeAngle.getValue());
-            int angle = (int)((Math.floor(stripeAngle.getValue()/15))*15);
-            double sw = width1.getValue()+width2.getValue();
-            double ih = sw;
-            double iw = sw;
-            if(angle > 0 && angle < 90) {
-                double theta = Math.toRadians(angle);
-                iw = sw/Math.cos(theta);
-                ih = sw/Math.sin(theta);
-            }
-
             g2.setPaint(noisePat);
             g2.fillRect(0,0,getWidth(),getHeight());
 
+            if(useStripe.isSelected()) {
+                int angle = (int)((Math.floor(stripeAngle.getValue()/15))*15);
+                double sw = width1.getValue()+width2.getValue();
+                double ih = sw;
+                double iw = sw;
+                if(angle > 0 && angle < 90) {
+                    double theta = Math.toRadians(angle);
+                    iw = sw/Math.cos(theta);
+                    ih = sw/Math.sin(theta);
+                }
 
-            BufferedImage img = new BufferedImage(
-                    (int) iw,
-                    (int) ih,
-                    BufferedImage.TYPE_INT_ARGB);
-            try {
-                pat = PatternPaint.create(img,img.getWidth(),img.getHeight());
-            } catch (IOException e) {
-                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+
+
+                BufferedImage img = new BufferedImage(
+                        (int) iw,
+                        (int) ih,
+                        BufferedImage.TYPE_INT_ARGB);
+                try {
+                    pat = PatternPaint.create(img,img.getWidth(),img.getHeight());
+                } catch (IOException e) {
+                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                }
+                Graphics2D gfx = img.createGraphics();
+                gfx.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
+
+
+                gfx.rotate(Math.toRadians(angle));
+
+                //stripe one (background)
+                gfx.setPaint(GraphicsUtil.toAWT(color1.getSelectedColor()));
+                gfx.fillRect(0, (int) -getHeight(), (int) getWidth(), (int) getHeight() * 2);
+
+                //stripe two (foreground)
+                gfx.setPaint(GraphicsUtil.toAWT(color2.getSelectedColor()));
+                gfx.fillRect((int)width1.getValue(), (int) -getHeight(), (int) width2.getValue(), (int) getHeight() * 2);
+                gfx.fillRect((int)(width1.getValue()*2+width2.getValue()), (int)-getHeight(), (int) width2.getValue(), (int) getHeight()*2);
+                gfx.rotate(-Math.toRadians(angle));
+
+
+                if(showPatternGrid.isSelected()) {
+                    gfx.setPaint(GraphicsUtil.toAWT(FlatColor.GRAY));
+                    gfx.drawRect(0,0,img.getWidth(),img.getHeight());
+                }
+
+                gfx.dispose();
+
+                g2.setOpacity(1.0-(noiseAmount.getValue()/100.0));
+                g2.setPaint(pat);
+                g2.fillRect(0,0,getWidth(),getHeight());
+            } else {
+                g2.setOpacity(1.0-(noiseAmount.getValue()/100.0));
+                g2.setPaint(color1.getSelectedColor());
+                g2.fillRect(0,0,getWidth(),getHeight());
             }
-            Graphics2D gfx = img.createGraphics();
-            gfx.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
-
-
-            gfx.rotate(Math.toRadians(angle));
-
-            //stripe one (background)
-            gfx.setPaint(GraphicsUtil.toAWT(color1.getSelectedColor()));
-            gfx.fillRect(0, (int) -getHeight(), (int) getWidth(), (int) getHeight() * 2);
-
-            //stripe two (foreground)
-            gfx.setPaint(GraphicsUtil.toAWT(color2.getSelectedColor()));
-            gfx.fillRect((int)width1.getValue(), (int) -getHeight(), (int) width2.getValue(), (int) getHeight() * 2);
-            gfx.fillRect((int)(width1.getValue()*2+width2.getValue()), (int)-getHeight(), (int) width2.getValue(), (int) getHeight()*2);
-            gfx.rotate(-Math.toRadians(angle));
-
-            if(showPatternGrid.isSelected()) {
-                gfx.setPaint(GraphicsUtil.toAWT(FlatColor.GRAY));
-                gfx.drawRect(0,0,img.getWidth(),img.getHeight());
-            }
-
-            gfx.dispose();
-
-            g2.setOpacity(1.0-(noiseAmount.getValue()/100.0));
-            g2.setPaint(pat);
-            g2.fillRect(0,0,getWidth(),getHeight());
 
             g2.setPaint(FlatColor.BLACK);
             g2.drawRect(0, 0, getWidth(), getHeight());
