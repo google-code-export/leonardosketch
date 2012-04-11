d26a74bf400bd75808118b24351165b1aa0b4967
joshmarinacci <joshmarinacci@localhost>
2012-04-10 19:47:30 -0700

    added tree headers

--- CoreModel/src/assetmanager/Query.java
+++ CoreModel/src/assetmanager/Query.java
@@ -15,12 +15,17 @@ class Query {
     public  final int x;
     public  final int y;
     private final String kind;
-    
+    private boolean selectable;
+
+    Query(String name, String kind) {
+        this(name,kind,-1,-1);
+    }
     Query(String name, String kind, int x, int y) {
         this.name = name;
         this.kind = kind;
         this.x = x;
         this.y = y;
+        this.selectable = true;
     }
 
     String getName() {
@@ -30,5 +35,12 @@ class Query {
     public List<Asset> execute(AssetDB db) {
         return db.getByKind(kind);
     }
-    
+
+    public void setSelectable(boolean selectable) {
+        this.selectable = selectable;
+    }
+
+    public boolean isSelectable() {
+        return selectable;
+    }
 }
