d4a6ff48f487c0c518b0a10c7314f9b606788986
josh <josh@joshuas-imac>
2012-04-12 14:40:22 -0700

    add NES palette add palette switching

--- Sketch/src/org/joshy/sketch/actions/swatches/ColorSwatchManager.java
+++ Sketch/src/org/joshy/sketch/actions/swatches/ColorSwatchManager.java
@@ -1,9 +1,12 @@
 package org.joshy.sketch.actions.swatches;
 
-import assetmanager.Asset;
 import assetmanager.AssetDB;
 import org.joshy.gfx.draw.FlatColor;
 import org.joshy.gfx.util.ArrayListModel;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 /**
  * Created by IntelliJ IDEA.
@@ -31,15 +34,26 @@ public class ColorSwatchManager {
             pal.add(FlatColor.BLUE);
             pal.save();
             pal.setName("Standard Palette");
+            pal.setEditable(true);
+
+            Palette pal2 = db.createPalette();
+            generateNESPalette(pal2);
+            pal2.save();
+            pal2.setName("NES");
+            pal2.setEditable(false);
         }
         palettes.addAll(db.getAllPalettes());
-        current = palettes.get(0);
+        current = palettes.get(1);
         swatchModel.clear();
         swatchModel.addAll(current.getColors());
     }
 
     public ArrayListModel<FlatColor> getSwatchModel() {
         return swatchModel;
+    }
+
+    public ArrayListModel<Palette> getPalettes() {
+        return palettes;
     }
 
     public void addSwatch(FlatColor color) {
@@ -49,4 +63,32 @@ public class ColorSwatchManager {
         swatchModel.addAll(current.getColors());
     }
 
+    private void generateNESPalette(Palette pal2) {
+        try {
+            for(String line : toLines(this.getClass().getResourceAsStream("nes.txt"))){
+                pal2.add(new FlatColor(line));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String[] toLines(InputStream inputStream) throws IOException {
+        InputStreamReader reader = new InputStreamReader(inputStream);
+        StringBuffer sb = new StringBuffer();
+        char[] buff = new char[1024];
+        while(true) {
+            int n = reader.read(buff);
+            if(n < 0) break;
+            sb.append(buff,0,n);
+        }
+
+        return sb.toString().split("\n");
+    }
+
+    public void setCurrentPalette(Palette palette) {
+        current = palette;
+        swatchModel.clear();
+        swatchModel.addAll(current.getColors());
+    }
 }
